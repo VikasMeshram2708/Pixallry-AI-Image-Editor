a84063a67c41a87cb5c5777b328006aa1ae43e06
VikasMeshram2708 <meshramvikas744@gmail.com>
2025-10-22 20:20:22 +0530

    added_navbar.

--- README.md
+++ README.md
@@ -1,36 +1,11 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+## Picallry
 
-## Getting Started
+- Picallry is an AI-powered image editing platform that empowers creators, designers, and businesses to transform, enhance, and personalize visuals effortlessly. Built for speed, precision, and creativity, Picallry combines advanced generative AI with an intuitive interface to deliver professional-grade edits in seconds.
 
-First, run the development server:
+### Vision:
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+- To redefine visual creativity through human-AI collaboration, enabling anyone to bring their visual ideas to life effortlessly.
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+### Target Audience:
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+- Content creators, photographers, e-commerce brands, marketing teams, and digital design professionals seeking fast and high-quality AI editing tools.
---
+++ components/navbar.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import { signIn, useSession } from "next-auth/react";
+import { useState } from "react";
+import { motion } from "motion/react";
+import { Menu, Sparkles, X } from "lucide-react";
+import { Button } from "./ui/button";
+
+export default function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const { data: session } = useSession();
+
+  function scrollToSection(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+      setIsMobileMenuOpen(false);
+    }
+  }
+
+  async function handleSubmit() {
+    if (session?.user) {
+      scrollToSection("editor");
+    } else {
+      await signIn("google");
+    }
+  }
+  return (
+    <motion.nav
+      initial={{ y: -100 }}
+      animate={{ y: 0 }}
+      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300
+      ${
+        isScrolled
+          ? "shadow-glass border-b border-card-border backdrop-blur-glass"
+          : "bg-transparent"
+      }
+      
+      `}
+    >
+      <div className="container  mx-auto p-4">
+        <div className="flex items-center justify-between">
+          {/* logo */}
+          <motion.div
+            className="flex items-center  cursor-pointer space-x-2"
+            whileHover={{ scale: 1.05 }}
+            onClick={() => scrollToSection("hero")}
+          >
+            <div className="relative">
+              <Sparkles
+                fill="transparent"
+                className="h-8 w-8 text-primary animate-glow-pluse"
+              />
+              <div className="absolute inset-0 h-8 w-8 text-secondary animate-glow-pulse opacity-50" />
+            </div>
+            <span className="text-2xl font-bold bg-gradient-primary !bg-clip-text text-transparent">
+              Pixallry AI
+            </span>
+          </motion.div>
+
+          {/* navigations */}
+          <div className="hidden md:flex items-center space-x-8">
+            <button
+              type="button"
+              className="text-foreground hover:text-primary transition-colors font-medium"
+              onClick={() => scrollToSection("features")}
+            >
+              Features
+            </button>
+            <button
+              type="button"
+              className="text-foreground hover:text-primary transition-colors font-medium"
+              onClick={() => scrollToSection("pricing")}
+            >
+              Pricing
+            </button>
+            <Button
+              variant={"hero"}
+              onClick={handleSubmit}
+              className="font-semibold w-full"
+            >
+              {session?.user ? "Launch App" : "Sign In"}
+            </Button>
+          </div>
+
+          {/* mobile menu */}
+          <button
+            type="button"
+            className="md:hidden text-foreground"
+            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+          >
+            {isMobileMenuOpen ? (
+              <X className="h-6 w-6" />
+            ) : (
+              <Menu className="h-6 w-6" />
+            )}
+          </button>
+        </div>
+        <motion.div
+          initial={false}
+          animate={{
+            height: isMobileMenuOpen ? "auto" : "0",
+            opacity: isMobileMenuOpen ? 1 : 0,
+          }}
+          className="md:hidden overflow-hidden"
+        >
+          <div className="py-4 space-y-4">
+            <button
+              type="button"
+              className="text-foreground hover:text-primary transition-colors font-medium"
+              onClick={() => scrollToSection("features")}
+            >
+              Features
+            </button>
+            <button
+              type="button"
+              className="text-foreground hover:text-primary transition-colors font-medium"
+              onClick={() => scrollToSection("pricing")}
+            >
+              Pricing
+            </button>
+            <Button
+              variant={"hero"}
+              onClick={handleSubmit}
+              className="font-semibold w-full"
+            >
+              {session?.user ? "Launch App" : "Sign In"}
+            </Button>
+          </div>
+        </motion.div>
+      </div>
+    </motion.nav>
+  );
+}
